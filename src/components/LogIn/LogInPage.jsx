b91598d8b17b0e95b05426b50bbb8968c973beb0
MZFTripty <zarinfatematripty@gmail.com>
2023-12-25 18:46:26 +0600

    add show or hide password

--- src/components/LogIn/LogInPage.jsx
+++ src/components/LogIn/LogInPage.jsx
@@ -2,56 +2,59 @@ import Lottie from "lottie-react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import logInAnimation from '../../Animations/Animation-logIn.json'
 import { FcGoogle } from "react-icons/fc";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import Swal from "sweetalert2";
 import { VscLoading } from "react-icons/vsc";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 function LogInPage() {
-    const {signInUser ,loading,setLoading,signInGoogle}= useContext(AuthContext)
+    const { signInUser, loading, setLoading, signInGoogle } = useContext(AuthContext)
     const navigate = useNavigate()
     const location = useLocation()
+    const [show, setShow] = useState(false)
+    const [length, setLength] = useState(0)
 
-    const handleGoogle =()=>{
+    const handleGoogle = () => {
         signInGoogle()
-        .then(res=>{
-            console.log(res.user)
-            navigate(location?.state ? location?.state : '/')
-        })
-        .catch(error=>{
-            console.log(error.message)
-        })
+            .then(res => {
+                console.log(res.user)
+                navigate(location?.state ? location?.state : '/')
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
     }
 
 
-    const handleLogIn = (e)=>{
+    const handleLogIn = (e) => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        signInUser(email,password)
-        .then(res=>{
-            console.log('success', res.user)
-            Swal.fire({
-                position: "center",
-                icon: "success",
-                title: "Successfully Signned In !",
-                showConfirmButton: false,
-                timer: 2000
-            });
-            form.reset()
-            navigate('/')
-        })
-        .catch(error =>{
-            console.log(error.message)
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: `${error.message}`,
-            });
-            setLoading(false)
-        })
+        signInUser(email, password)
+            .then(res => {
+                console.log('success', res.user)
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Successfully Signned In !",
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+                form.reset()
+                navigate('/')
+            })
+            .catch(error => {
+                console.log(error.message)
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: `${error.message}`,
+                });
+                setLoading(false)
+            })
     }
     return (
         <div className="flex flex-col lg:flex-row-reverse my-12 justify-center items-center gap-20 p-4 max-w-4xl mx-auto rounded-2xl">
@@ -61,12 +64,19 @@ function LogInPage() {
             <div className="shadow-lg rounded-lg p-4">
                 <p className="text-center font-semibold text-2xl pb-3 text-purple-600">Please Sign In!!</p>
                 <form onSubmit={handleLogIn} className="flex flex-col items-center gap-y-3">
-                    
+
                     <input type="email" name='email' placeholder="Enter Email" className="input input-bordered w-full max-w-xs" />
-                    <input type="password" name='password' placeholder="Enter Password" className="input input-bordered w-full max-w-xs" />
+                    <div className="relative w-full">
+                        <input type={show ? 'text' : 'password'} onChange={(e) => setLength(e.target.value.length)} name='password' placeholder="Enter Password" className="input input-bordered w-full max-w-xs" />
+                        {
+                            length === 0 ? <p></p> :
+                                show ? <p onClick={() => setShow(false)}><FaEyeSlash className="absolute text-xl top-3 right-2 text-black" /></p> :
+                                    <p onClick={() => setShow(true)}><FaEye className="absolute text-xl top-3 right-2 text-black" /></p>
+                        }
+                    </div>
                     {
                         loading ? <button className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs hover:text-black  '><VscLoading className="animate-spin text-3xl" /></button>
-                        : <input type="submit" className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs ' value="sign in" />
+                            : <input type="submit" className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs ' value="sign in" />
                     }
                 </form>
                 <p className="pt-3 text-center"> Have no Account? <Link to={'/signuppage'} className="font-semibold">Sign Up</Link> </p>
--- src/components/LogIn/SignUpPage.jsx
+++ src/components/LogIn/SignUpPage.jsx
@@ -2,16 +2,20 @@ import Lottie from "lottie-react"
 import logInAnimation from '../../Animations/Animation-logIn.json'
 import { Link, useNavigate } from "react-router-dom"
 import { FcGoogle } from "react-icons/fc";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { updateProfile } from "firebase/auth";
 import { VscLoading } from "react-icons/vsc";
 import Swal from "sweetalert2";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 function SignUpPage() {
-    const { createUser, loading, setLoading,signInGoogle } = useContext(AuthContext)
+    const { createUser, loading, setLoading, signInGoogle } = useContext(AuthContext)
     const navigate = useNavigate()
+    const [show,setShow]=useState(false)
+    const [length,setLength]=useState(0)
+    console.log(length)
 
     const handleSignUp = (e) => {
         e.preventDefault();
@@ -48,15 +52,15 @@ function SignUpPage() {
             })
 
     }
-    const handleGoogle=()=>{
+    const handleGoogle = () => {
         signInGoogle()
-        .then(res=>{
-            console.log(res)
-            navigate('/')
-        })
-        .catch(error=>{
-            console.log(error.message)
-        })
+            .then(res => {
+                console.log(res)
+                navigate('/')
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
     }
     return (
         <div className="flex flex-col lg:flex-row-reverse my-12 justify-center items-center gap-20 p-4 max-w-4xl mx-auto rounded-2xl">
@@ -69,10 +73,17 @@ function SignUpPage() {
                     <input type="text" name='userName' placeholder="Enter User Name" className="input input-bordered w-full max-w-xs" />
                     <input type="text" name='photo' placeholder="Enter Photo URL" className="input input-bordered w-full max-w-xs" />
                     <input type="email" name='email' placeholder="Enter Email" className="input input-bordered w-full max-w-xs" />
-                    <input type="password" name='password' placeholder="Enter Password" className="input input-bordered w-full max-w-xs" />
+                    <div className="relative w-full">
+                        <input type={show?'text':'password'} onChange={(e)=> setLength(e.target.value.length)} name='password' placeholder="Enter Password" className="input input-bordered w-full max-w-xs" />
+                        {
+                            length===0?<p></p>:
+                            show?<p onClick={()=> setShow(false)}><FaEyeSlash className="absolute text-xl top-3 right-2 text-black" /></p>:
+                            <p onClick={()=> setShow(true)}><FaEye className="absolute text-xl top-3 right-2 text-black" /></p>
+                        }
+                    </div>
                     {
                         loading ? <button className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs hover:text-black  '><VscLoading className="animate-spin text-3xl" /></button>
-                            : <input type="submit" className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs hover:text-black ' value="Sign Up" />
+                            : <button className='btn bg-purple-800 text-white hover:text-lg w-full max-w-xs hover:text-black '>Sign Up</button>
                     }
                 </form>
                 <p className="pt-3 ">Already Have an Account? <Link to={'/loginpage'} className="font-semibold">Sign In</Link> </p>
